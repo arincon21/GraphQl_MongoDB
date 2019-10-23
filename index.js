4039c5454668f48cf46330108a403ded98cad138
Alvaro Rincón <alvaro@joonik.com>
2019-10-23 15:41:53 -0500

    clase 4

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const { graphql, buildSchema } = require('graphql')
+'use strict'
+
+const { buildSchema } = require('graphql')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
-// Definiendo el esquema
-const schema = buildSchema(`
-    type Query {
-        "Retorna un saludo"
-        hello: String
-    }
-`)
-
-// Configurar los resolvers
-const resolvers = {
-    hello: () => 'Hola mundo',
-}
+// definiendo el esquema
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+)
 
 app.use('/api', gqlMiddleware({
   schema: schema,
@@ -24,4 +24,6 @@ app.use('/api', gqlMiddleware({
   graphiql: true
 }))
 
-app.listen(port, () => console.log(`Server is listening at http://localhost:${port}/api`))
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
